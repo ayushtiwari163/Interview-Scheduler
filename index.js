af67774f11839745a9243277bc8b068bf7a6de25
Ayush Tiwari <ayushtiwari163@gmail.com>
2021-12-18 21:12:20 +0530

    prepare for deployement

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express =  require('express')
-const app =  express()
-const port = process.env.PORT || 8000
-require('./db');
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 8000;
+require("./db");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //console.log(process.env.MONGO_URL);
-app.use('/users',require('./routes/users'))
-app.use('/interviews', require('./routes/interview'))
-app.listen(port,() => {
-    console.log("app is running on port 8000");
-})
+app.use("/users", require("./routes/users"));
+app.use("/interviews", require("./routes/interview"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log("app is running on port 8000");
+});
